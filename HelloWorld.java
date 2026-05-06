11ae1ffb7fc7827f9db7fbd2d44ca1ba3a2b852d
ganesh-meher <ganeshmeher6614@gmail.com>
2026-05-06 21:56:20 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,17 @@
-public class HelloWorld {
+public class PrimeNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        for (int i = 1; i <= 50; i++) {
+            if (i <= 1) continue;
+            boolean isPrime = true;
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
