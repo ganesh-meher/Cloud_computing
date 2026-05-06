b7fcbbfdc985d4328ebdfb7f3c78f2d2244811b0
ganesh-meher <ganeshmeher6614@gmail.com>
2026-05-06 21:35:26 +0530

    Update Prime_no.py

--- Prime_no.py
+++ Prime_no.py
@@ -2,15 +2,12 @@ def is_prime(num):
     if num <= 1:
         return False
     
-    for i in range(2, num):
+    for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
             return False
     
     return True
 
-num = int(input())
-
-if is_prime(num):
-    print("Prime number")
-else:
-    print("Not a prime number")
+for i in range(0, 51):
+    if is_prime(i):
+        print(i)
